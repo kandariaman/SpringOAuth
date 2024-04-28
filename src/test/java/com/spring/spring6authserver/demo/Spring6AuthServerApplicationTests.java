b9b3f4c445b2510e9d003bb6c4543f5d5c1e605b
Aman Kandari <kandariaman24@gmail.com>
2024-04-28 17:59:02 +0530

    Git OAuth2 project

---
+++ src/test/java/com/spring/spring6authserver/demo/Spring6AuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.spring6authserver.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6AuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
